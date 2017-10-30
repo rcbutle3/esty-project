6ce7c023af82ff403450b173f1fcf13b4cf0acbc
Robin Butler <rcbutle3@aggies.ncat.edu>
2017-10-30 10:09:19 -0400

    Corrected Version
    Corrections Made to Files.

--- main.js
+++ main.js
@@ -36,25 +36,53 @@ function question2 () {
 
 // 3: Which item has a "GBP" currency code? Display it's name and price.
 function question3 () {
-  // Answer:
+    let gbp = [];
+    let price = 0;
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].currency_code === "GBP") {
+            price = data[i].price;
+            gbp.push(data[i].title);
+        }
+    } console.log(gbp + " costs " + price + " pounds.");
 }
 
 
 // 4: Display a list of all items who are made of wood.
 function question4 () {
-  // Answer:
+    let woodItem = [];
+    for (let i = 0; i < data.length; i++) {
+        for (let f = 0; f < data[i].materials.length; f++) {
+        if (data[i].materials[f] === "wood") {
+            woodItem.push(data[i].title + " is made of wood.");
+            }
+        }
+    } console.log(woodItem.join("\r\n"));
 }
 
 
-// 5: Which items are made of eight or more materials?
+// 5: Which items are made of eight or more materials? 
 //    Display the name, number of items and the items it is made of.
 function question5 () {
-  // Answer:
+    let eightOrMore = [];
+    let matList = [];
+    for (let i = 0; i < data.length; i++) {
+        let totalMats = data[i].materials.length;
+        if (data[i].materials.length >= 8) {
+            let matList = data[i].materials.join("\r\n" + "- ");
+            eightOrMore.push(data[i].title + " has " + totalMats + " materials:" + "\r\n" + "- " + matList);
+        }
+    } console.log(eightOrMore.join("\r\n")); 
 }
 
 
 // 6: How many items were made by their sellers?
 // Answer:
 function question6 () {
-  // Answer:
-}
+    let madeBySeller = [];
+    let madeBySellerCount = 0;   
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].who_made === "i_did") {
+            madeBySellerCount++;
+            madeBySeller = madeBySellerCount;
+        }
+    } console.log(madeBySeller + " items were made by their sellers.");
